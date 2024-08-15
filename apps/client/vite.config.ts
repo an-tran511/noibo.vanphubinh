d6c78b59ca0cd91998c285e52bc3686e9d55ab9c
An <an.tran511@outlook.com>
2024-08-15 14:51:47 +0700

    [client]: install tanstack router & query

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import million from "million/compiler";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [million.vite({ auto: true }), react()],
+	plugins: [TanStackRouterVite(), million.vite({ auto: true }), react()],
 	server: {
 		proxy: {
 			"/api": {
